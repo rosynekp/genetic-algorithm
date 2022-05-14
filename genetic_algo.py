c7808bba54b595ed587adea3aac58b91fbbbcf63
rosynekp <80347379+rosynekp@users.noreply.github.com>
2022-05-14 11:03:17 -0500

    initial commit

---
+++ genetic_algo.py
@@ -0,0 +1,35 @@
+from functools import partial
+from problems import knapsack
+from algorithms import bruteforce, genetic
+from utils.analyze import timer
+
+things = knapsack.generate_things(50)
+
+
+weight_limit = 1000
+
+# print("Weight Limit: %dkg" % weight_limit)
+# print("")
+# print("BRUTEFORCE")
+# print("----------")
+#
+# with timer():
+# 	result = bruteforce(things, weight_limit)
+#
+# knapsack.print_stats(result[1])
+
+print("")
+print("GENETIC ALGORITHM")
+print("----------")
+
+with timer():
+	population, generations = genetic.run_evolution(
+		populate_func=partial(genetic.generate_population, size=10, genome_length=len(things)),
+		fitness_func=partial(knapsack.fitness, things=things, weight_limit=weight_limit),
+		fitness_limit=1000,
+		printer=True,
+		generation_limit=100
+	)
+
+sack = knapsack.from_genome(population[0], things)
+knapsack.print_stats(sack)
